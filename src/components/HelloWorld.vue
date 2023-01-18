34d514fe97d89840c19a0cae4b54f54eb5aea0a3
goyeh98 <yogaafriend@gmail.com>
2023-01-18 16:25:22 +0700

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,83 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container mt-5 utama">
+    <header>
+      <h3>Diari Jajan Februari 2021</h3>
+      <p class="text-center">Pengeluaran Bulan ini</p>
+      <div class="modal-button d-flex justify-content-center">
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">TAMBAH ITEM</button>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-sm">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Entri</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <form>
+                  <div class="mb-3">
+                    <label for="recipient-name" class="col-form-label">Nama :</label>
+                    <input type="text" class="form-control" id="recipient-name" />
+                  </div>
+                  <div class="mb-3">
+                    <label for="recipient-name" class="col-form-label">Harga :</label>
+                    <input type="number" class="form-control" id="recipient-name" />
+                  </div>
+                </form>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                <button type="button" class="btn btn-primary">Kirim</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container mt-3">
+        <div class="row">
+          <div class="card todo col-3" v-for="(genderUsers, gender) in groupedUsers">
+            <div class="card-body">
+              <h3 class="card-title">{{ gender }}</h3>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item" v-for="user in genderUsers">{{ user.jam }} {{ user.nama }} Rp.{{ user.pengeluaraan }}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <article class="container"></article>
   </div>
 </template>
 
 <script>
+// import userData from './data.json';
+import _ from 'lodash';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    // Import the JSON file
+    import('./data.json').then((jsonData) => {
+      this.users = jsonData.title;
+    });
+  },
+  computed: {
+    groupedUsers() {
+      return _.groupBy(this.users, 'tanggal');
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.utama {
+  background-color: aliceblue;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import store from './store';
+import 'bootstrap/dist/css/bootstrap.css';
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js';
 
-createApp(App).use(store).mount('#app')
+createApp(App).use(store).use(bootstrap).mount('#app');
